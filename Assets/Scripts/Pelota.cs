571d1063683ffbdbb09e2687c607b63591611417
Hector Robles <hector@bwteconecta.com>
2022-03-03 13:41:10 -0600

    Clone reds

--- Assets/Scripts/Pelota.cs
+++ Assets/Scripts/Pelota.cs
@@ -11,11 +11,14 @@ public class Pelota : MonoBehaviour
     [SerializeField]
     private Rigidbody rb;
     public Vector3 vector = new Vector3(0,600,0);
+    public Vector3 cubepos;
     public float velocidadX = 500;
     public float velocidadY = 500;
     private int puntuacion;
     public Text puntos;
     public Text info;
+    private string name;
+    private GameObject cube;
     // Start is called before the first frame update
     void Start()
     {
@@ -79,6 +82,24 @@ public class Pelota : MonoBehaviour
 
     void OnTriggerEnter(Collider c){
        if (c.transform.tag == "Rojo"){
+         name = c.transform.name;
+         cube = GameObject.Find(name);
+         cubepos = cube.transform.position;
+         cubepos.x += Random.Range(-10,10);
+         if (cubepos.x < -23){
+           cubepos.x += Random.Range(3,10);
+         }
+         else if(cubepos.x > 20){
+           cubepos.x -= Random.Range(3,10);
+         }
+         cubepos.y += Random.Range(-5,5);
+         if(cubepos.y > 2.27){
+           cubepos.y -= Random.Range(2,7);
+         }
+         else if(cubepos.y < -11){
+           cubepos.y += Random.Range(2,7);
+         }
+         Instantiate(cube,cubepos,cube.transform.rotation);
          AddOne();
        }
        if (c.gameObject.layer == 3){
@@ -92,14 +113,12 @@ public class Pelota : MonoBehaviour
    }
 
    void OnTriggerStay(Collider c){
-       print("Trigger Stay");
 
 
 
    }
 
    void OnTriggerExit(Collider c){
-       print("Trigger Exit");
 
 
    }
@@ -124,6 +143,7 @@ public class Pelota : MonoBehaviour
 
    void delText(){
    info.text = "";
- }
+   }
+
 
 }
