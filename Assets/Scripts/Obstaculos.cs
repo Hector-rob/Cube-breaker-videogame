774c2733ef08e9c567fe46cf3c6783e0c6da8b68
Hector Robles <hector@bwteconecta.com>
2022-03-02 21:54:34 -0600

    Mover los verdes

--- Assets/Scripts/Obstaculos.cs
+++ Assets/Scripts/Obstaculos.cs
@@ -6,40 +6,70 @@ public class Obstaculos : MonoBehaviour
 {
 
     public int speed = 3;
-    private bool flag = false;
-    private bool flag2 = false;
+    private bool flagAbajo = false;
+    private bool flag2Abajo = false;
+    private bool flagArriba = false;
+    private bool flag2Arriba = false;
     private int recorrido = 0;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
 
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-      if(transform.position.y <= -11 && transform.position.x <= 17){
-        transform.Translate(5*Time.deltaTime*speed,0,0);
+    void Update(){
+      if(gameObject.tag == "CubitoAbajo"){
+        if(transform.position.y <= -11 && transform.position.x <= 17){
+          transform.Translate(5*Time.deltaTime*speed,0,0);
+        }
+        else if (transform.position.x >= 17 && transform.position.y <= 2.27){
+          transform.Translate(0,5*Time.deltaTime*speed,0);
+          flagAbajo = true;
+        }
+        else if (flagAbajo == true){
+            transform.Translate(-5*Time.deltaTime*speed,0,0);
+            if(transform.position.x <= -20.6){
+              flagAbajo = false;
+              flag2Abajo = true;
+            }
+        }
+        else if (flag2Abajo == true){
+          transform.Translate(0,-5*Time.deltaTime*speed,0);
+        }
+        else if (flagAbajo == false && transform.position.y <= -11){
+          transform.Translate(0,-5*Time.deltaTime*speed,0);
+        }
       }
-      else if (transform.position.x >= 17 && transform.position.y <= 2.27){
-        transform.Translate(0,5*Time.deltaTime*speed,0);
-        flag = true;
-      }
-      else if (flag == true){
+      else{
+        if(transform.position.x > -20.6 && transform.position.y <= 2.27 && flagArriba == false){
           transform.Translate(-5*Time.deltaTime*speed,0,0);
-          if(transform.position.x <= -20.6){
-            flag = false;
-            flag2 = true;
+        }
+        else if (transform.position.x <= -20.6 && transform.position.y <= 2.27 && flagArriba == false){
+          transform.Translate(0,-5*Time.deltaTime*speed,0);
+          if(transform.position.y <= -11){
+            flagArriba = true;
           }
+        }
+        else if (flagArriba == true && flag2Arriba == false){
+          transform.Translate(5*Time.deltaTime*speed,0,0);
+            if(transform.position.x >= 17){
+              flag2Arriba = true;
+            }
+        }
+        else if (transform.position.x >= -11 && flag2Arriba == true ){
+          print("ntre");
+          transform.Translate(0,5*Time.deltaTime*speed,0);
+        }
+        else if (flagArriba == true && transform.position.y >= 2.27){
+          transform.Translate(0,5*Time.deltaTime*speed,0);
+        }
 
       }
-      else if (flag2 == true){
-        transform.Translate(0,-5*Time.deltaTime*speed,0);
-        //flag = false;
-      }
-      else if (flag == false && transform.position.y <= -11){
-        transform.Translate(0,-5*Time.deltaTime*speed,0);
-      }
+
+
+
+
     }
+
 }
