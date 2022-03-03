0c6f6de9ae8495e535aff391b97ffdeeffc4d792
Hector Robles <hector@bwteconecta.com>
2022-03-02 20:59:17 -0600

    move in rectangle

--- Assets/Scripts/Obstaculos.cs
+++ Assets/Scripts/Obstaculos.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Obstaculos : MonoBehaviour
 {
 
-    public int speed = 1;
-    private bool switch1;
-    private bool switch2;
+    public int speed = 3;
+    private bool flag = false;
+    private bool flag2 = false;
+    private int recorrido = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,23 +19,27 @@ public class Obstaculos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      while (!switch1 && !switch2){
-        transform.Translate(3*Time.deltaTime,0,0);
+      if(transform.position.y <= -11 && transform.position.x <= 17){
+        transform.Translate(5*Time.deltaTime*speed,0,0);
       }
-      if(transform.position.x <= -20){
-        switch1 = true;
-        transform.Translate(3*Time.deltaTime,0,0);
+      else if (transform.position.x >= 17 && transform.position.y <= 2.27){
+        transform.Translate(0,5*Time.deltaTime*speed,0);
+        flag = true;
+      }
+      else if (flag == true){
+          transform.Translate(-5*Time.deltaTime*speed,0,0);
+          if(transform.position.x <= -20.6){
+            flag = false;
+            flag2 = true;
+          }
 
       }
-      else if(transform.position.x >= 17){
-        switch2 = true;
-        transform.Translate(-3*Time.deltaTime,0,0);
-
-
+      else if (flag2 == true){
+        transform.Translate(0,-5*Time.deltaTime*speed,0);
+        //flag = false;
       }
-
-
-
+      else if (flag == false && transform.position.y <= -11){
+        transform.Translate(0,-5*Time.deltaTime*speed,0);
+      }
     }
-
 }
